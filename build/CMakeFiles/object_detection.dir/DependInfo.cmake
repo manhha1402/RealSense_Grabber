98ac632f16703669fe57058fe73f7cc75a0d1508
Manh Ha Hoang <manhha1402@gmail.com>
2017-11-20 11:53:08 +0100

    update

--- build/CMakeFiles/object_detection.dir/DependInfo.cmake
+++ build/CMakeFiles/object_detection.dir/DependInfo.cmake
@@ -4,41 +4,36 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/hahoang/object_detection/realsense2_grabber/main.cpp" "/home/hahoang/object_detection/realsense2_grabber/build/CMakeFiles/object_detection.dir/main.cpp.o"
-  "/home/hahoang/object_detection/realsense2_grabber/realsense.cpp" "/home/hahoang/object_detection/realsense2_grabber/build/CMakeFiles/object_detection.dir/realsense.cpp.o"
-  "/home/hahoang/object_detection/realsense2_grabber/realsense_utils.cpp" "/home/hahoang/object_detection/realsense2_grabber/build/CMakeFiles/object_detection.dir/realsense_utils.cpp.o"
+  "/home/jcl-stud/object-detection/RealSense_Grabber/main.cpp" "/home/jcl-stud/object-detection/RealSense_Grabber/build/CMakeFiles/object_detection.dir/main.cpp.o"
+  "/home/jcl-stud/object-detection/RealSense_Grabber/realsense.cpp" "/home/jcl-stud/object-detection/RealSense_Grabber/build/CMakeFiles/object_detection.dir/realsense.cpp.o"
+  "/home/jcl-stud/object-detection/RealSense_Grabber/realsense_utils.cpp" "/home/jcl-stud/object-detection/RealSense_Grabber/build/CMakeFiles/object_detection.dir/realsense_utils.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
-  "DISABLE_DAVIDSDK"
-  "DISABLE_DSSDK"
-  "DISABLE_ENSENSO"
-  "DISABLE_LIBUSB_1_0"
-  "DISABLE_OPENNI2"
+set(CMAKE_TARGET_DEFINITIONS
   "DISABLE_PCAP"
   "DISABLE_PNG"
-  "DISABLE_RSSDK"
   "EIGEN_USE_NEW_STDVECTOR"
   "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
   "FLANN_STATIC"
   "qh_QHpointer"
   )
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/include/vtk-5.8"
-  "/usr/local/include"
-  "/usr/local/include/opencv"
-  "/usr/local/include/pcl-1.8"
-  "/usr/include/eigen3"
-  "/usr/include/ni"
-  )
-
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
-# Fortran module output directory.
-set(CMAKE_Fortran_TARGET_MODULE_DIR "")
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/usr/include/vtk-5.8"
+  "/usr/include/pcl-1.7"
+  "/usr/include/eigen3"
+  "/usr/include/ni"
+  "/usr/include/openni2"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #include <boost/make_shared.hpp>
 #include <pcl/common/common.h>
 #include "realsense.h"
-
+#include "realsense_utils.h"
 using namespace std;
 void view(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud)
 {
@@ -31,58 +31,16 @@ int main() try
    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
    pcl::io::loadPCDFile("../meanregistration_cloud.pcd",*cloud);
     realsense dev;
-    /*Compute the 3x3 covariance matrix of a given set of points.
-   * The result is returned as a Eigen::Matrix3f.
-   * Note: the covariance matrix is not normalized with the number ofpoints
-   */
-   /*
-   Eigen::Vector4f pcaCentroid;
-   pcl::compute3DCentroid(*cloud,pcaCentroid);
-   Eigen::Matrix3f covariance;
-   pcl::computeCovarianceMatrixNormalized(*cloud,pcaCentroid,covariance);
-   //Compute eigenvectors and eigenvalues of covariance matrix
-   Eigen::SelfAdjointEigenSolver<Eigen::Matrix3f> eigen_solver(covariance, Eigen::ComputeEigenvectors);
-   //Eigen vectors
-   Eigen::Matrix3f eigenVectorsPCA = eigen_solver.eigenvectors();
-   cout<<eigenVectorsPCA<<endl;
-   /// This line is necessary for proper orientation in some cases. The numbers come out the same without it, but
-/// the signs are different and the box doesn't get correctly oriented in some cases.
-   eigenVectorsPCA.col(2) = eigenVectorsPCA.col(0).cross(eigenVectorsPCA.col(1));
-   // Transform the original cloud to the origin point where the principal components correspond to the axes.
-   Eigen::Matrix4f projectionTransform(Eigen::Matrix4f::Identity());
-   projectionTransform.block<3,3>(0,0) = eigenVectorsPCA.transpose();
-   projectionTransform.block<3,1>(0,3) = -1.f * (projectionTransform.block<3,3>(0,0) * pcaCentroid.head<3>());
-   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudPointsProjected (new pcl::PointCloud<pcl::PointXYZRGB>);
-   pcl::transformPointCloud(*cloud,*cloudPointsProjected,projectionTransform);
-   // Get the minimum and maximum points of the transformed cloud.
-   pcl::PointXYZRGB minPoint, maxPoint;
-pcl::getMinMax3D(*cloudPointsProjected, minPoint, maxPoint);
-const Eigen::Vector3f meanDiagonal = 0.5f*(maxPoint.getVector3fMap() + minPoint.getVector3fMap());
-//Finally, the quaternion is calculated using the eigenvectors (which determines how the final box gets rotated),
-//and the transform to put the box in correct location is calculated.
-//The minimum and maximum points are used to determine the box width, height, and depth.
-const Eigen::Quaternionf bboxQuaternion(eigenVectorsPCA); //Quaternions are a way to do rotations
-const Eigen::Vector3f bboxTransform = eigenVectorsPCA * meanDiagonal + pcaCentroid.head<3>();
+    dev.printInformation();
+   while(dev.isConnected())
+   {
+       dev.wait_for_frames();
+        auto point_cloud = rs_utils::get_point_cloud(dev);
+        // Visualize the point cloud
+        auto viewer = rs_utils::visualize_rgb_pc(point_cloud);
+        rs_utils::loop_viewer(viewer);
+   }
 
-cout<<"pcaCentroid"<<endl<<pcaCentroid<<endl;
-   cout<<"covariance"<<endl<<covariance<<endl;
-   cout<<"eigenVectorsPCA"<<endl<<eigenVectorsPCA<<endl;
-   cout<<"bboxTransform"<<endl<<bboxTransform<<endl;
-   //cout<<"bboxQuaternion"<<endl<<bboxQuaternion<<endl;
-
-   boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
-   viewer.reset(new pcl::visualization::PCLVisualizer);
-     pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
-
-    viewer->addPointCloud(cloud,rgb, "output");
- viewer->addCube(bboxTransform, bboxQuaternion, maxPoint.x - minPoint.x, maxPoint.y - minPoint.y, maxPoint.z - minPoint.z);
-
-          while (!viewer->wasStopped ())
-             {
-               viewer->spinOnce ();
-             }
-
-*/
     return 0;
 }
 catch (const rs2::error & e)
